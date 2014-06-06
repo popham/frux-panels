1b1ddab59282542144aa11bd823129656efd5357
Tim Popham <popham@uw.edu>
2014-06-06 16:58:08 -0700

    Refactor to mixins and revised frux-list

--- src/install/group.jsx
+++ src/install/group.jsx
@@ -1,10 +1,14 @@
 /** @jsx react.DOM */
 
 var Group = require('../Group');
+var Store = require('frux-list/lib/Store');
 
 module.exports = function (element, dragBus) {
-    var panelStore = new List();
-    var group = <Group panelStore={panelStore} dragBus={dragBus} />;
+    var panelsStore = new Store();
+    var group = <Group panelsPublish={panelsStore.publish}
+                       panelsAct={panelsStore.act}
+                       dragBus={dragBus} />
+
     react.renderComponent(group, element);
 
     return group;
