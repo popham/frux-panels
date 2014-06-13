1785f852ecbbb08b616aca9b4a152d113fff3b82
Tim Popham <popham@uw.edu>
2014-06-12 20:53:35 -0700

    Dojo profile

---
+++ profile.js
@@ -0,0 +1,7 @@
+var profile = {
+    resourceTags : {
+        amd : function (filename, mid) {
+            return /^frux-panels\/lib\//.test(mid) || /^frux-panels\/index/.test(mid);
+        }
+    }
+};
