a7849d1e30db65d2be420f4447784df13e57f9c6
Tim Popham <popham@uw.edu>
2014-06-15 14:48:08 -0700

    Add a missed require dependency

--- src/mixin/panelsPublish.js
+++ src/mixin/panelsPublish.js
@@ -1,4 +1,4 @@
-define(['frux-list'], function (list) {
+define(['react', 'frux-list'], function (list) {
     return {
         propTypes : {
             panelsPublish : react.PropTypes.instanceOf(list.Publish).isRequired
