5c256784082f31c959021c63b9d2ba575ef10f99
Tim Popham <popham@uw.edu>
2014-06-15 21:46:34 -0700

    Insertion point icon dimensioning

--- src/mixin/groupMember.js
+++ src/mixin/groupMember.js
@@ -1,6 +1,5 @@
 define(['react', 'frux-list', '../DragBus'], function (
          react,        list,      DragBus) {
-
     return {
         propTypes : {
             panelsAct : react.PropTypes.instanceOf(list.Act).isRequired,
