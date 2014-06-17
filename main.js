9d9f05c442a1a465aae0950fa760dfdbe4faa3b6
Tim Popham <popham@uw.edu>
2014-06-17 15:15:51 -0700

    Expose custom store

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-define(['./lib/DragBus', './lib/Insertion', './lib/install/group', './lib/mixin/index'], function (
-               DragBus,         Insertion,                 group,         mixin){
+define(['./lib/DragBus', './lib/Insertion', './lib/Store', './lib/install/group', './lib/mixin/index'], function (
+               DragBus,         Insertion,         Store,                 group,         mixin){
     return {
         DragBus : DragBus,
         Insertion : Insertion,
+        Store : Store,
         group : group,
         mixin : mixin
     };
