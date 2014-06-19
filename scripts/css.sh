6ddf7ffaf2a5daf815b4db23c02ac192727643cf
Tim Popham <popham@uw.edu>
2014-06-18 17:38:11 -0700

    Normalization css

---
+++ scripts/css.sh
@@ -0,0 +1,6 @@
+node ./node_modules/.bin/lessc --include-path=./node_modules \
+    less/basic.less css/basic.css & \
+node ./node_modules/.bin/lessc --include-path=./node_modules \
+    less/glyphicons.less css/glyphicons.css & \
+node ./node_modules/.bin/lessc --include-path=./node_modules/bootstrap/less \
+    ./node_modules/bootstrap/less/normalize.less css/normalize.css
