7b71f13f58c08a44f762facf0fa7d9dbc4d8d04b
Tim Popham <popham@uw.edu>
2014-06-26 11:51:44 -0700

    Fixed incorrect filename

--- scripts/css.sh
+++ scripts/css.sh
@@ -1,4 +1,4 @@
-cp less/variables node_modules/bootstrap/less
+cp less/variables.less node_modules/bootstrap/less
 
 node ./node_modules/.bin/lessc --include-path=./node_modules \
     less/basic.less css/basic.css & \
