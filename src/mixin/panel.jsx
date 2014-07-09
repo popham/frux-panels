231dc7c09e203f1bea7b38ab46751e37fd6a8c4e
Tim Popham <popham@uw.edu>
2014-07-09 10:07:27 -0700

    Migrate to capabilities UI mixin

--- src/mixin/panel.jsx
+++ src/mixin/panel.jsx
@@ -1,58 +1,11 @@
 /** @jsx react.DOM */
 
-define(['react', './groupMember'], function (
-         react,     groupMember) {
-    /**
-     * Default policies
-     */
-    function headerUninstaller(array) {
-        array.push(
-            <header className="title-bar">
-                <div>
-                    <span className="glyphicon glyphicon-remove"
-                          onClick={this.uninstallHandler} />
-                </div>
-                <p>{this.state.title}</p>
-            </header>
-        );
+define(['react', '../title/icon/index', './groupMember'], function (
+         react,            icon,           groupMember) {
 
-        return array;
-    }
-
-    function contentTemplateMethod(array) {
-        if (this.content) {
-            array.push(
-                <div className="body-template">{this.content()}</div>
-            );
-        }
-
-        return array;
-    }
-
-    function footerIconicTriggers(array) {
-        function iconify(icon) {
-            return (
-                <li>
-                    <img height={this.state.iconHeight}
-                         width={this.state.iconWidth}
-                         src={icon.url}
-                         onClick={icon.onClick} />
-                </li>
-            );
-        }
-
-        if (this.state.icons.length > 0) {
-            array.push(
-                <footer className="iconic-triggers">
-                    <ul>
-                        {this.state.icons.map(iconify.bind(this))}
-                    </ul>
-                </footer>
-            );
-        }
-
-        return array;
-    }
+    var Close = icon.Close;
+    var Unmount = icon.Unmount;
+    var Fork = icon.Fork;
 
     /**
      * Externally supplied state:
@@ -64,30 +17,90 @@ define(['react', './groupMember'], function (
         propTypes : {
             headerPolicy : react.PropTypes.func,
             contentPolicy : react.PropTypes.func,
-            footerPolicy : react.PropTypes.func,
-            className : react.PropTypes.string
+            footerPolicy : react.PropTypes.func
         },
 
-        getDefaultProps : function () { return {
-            headerPolicy : headerUninstaller,
-            contentPolicy : contentTemplateMethod,
-            footerPolicy : footerIconicTriggers,
-            className : 'panel'
+        getInitialState : function () { return {
+            isMounted : true
         }; },
 
-        uninstall : function () {
+        className : function () {
+            var cls = ['panel'];
+            if (this.state.isMounted) { cls.push('mounted'); }
+
+            return cls.join(' ');
+        },
+
+        defaultHeader : function(array) {
+            array.push(
+                <header className="title-bar">
+                  <div>
+                    <Unmount className="icon" onClick={this.unmount} />
+                    <Fork className="icon" onClick={this.fork} />
+                    <Close className="icon" onClick={this.close} />
+                  </div>
+                  <p>{this.state.title}</p>
+                </header>
+            );
+
+            return array;
+        },
+
+        defaultContent : function (array) {
+            if (this.content) {
+                array.push(<div className="content" />);
+            }
+
+            return array;
+        },
+
+        defaultFooter : function (array) {
+            function iconify(icon) {
+                return (
+                    <li>
+                      <img height={this.state.iconHeight}
+                          width={this.state.iconWidth}
+                          src={icon.url}
+                          onClick={icon.onClick} />
+                    </li>
+                );
+            }
+
+            if (this.state.icons.length > 0) {
+                array.push(
+                    <footer className="iconic-triggers">
+                      <ul>
+                          {this.state.icons.map(iconify.bind(this))}
+                      </ul>
+                    </footer>
+                );
+            }
+
+            return array;
+        },
+
+        remove : function () {
             this.props.panelsAct.uninstall.push(this.props.key);
         },
 
         render : function () {
             var children = [];
 
-            this.props.headerPolicy.bind(this)(children);
-            this.props.contentPolicy.bind(this)(children);
-            this.props.footerPolicy.bind(this)(children);
+            var header = this.defaultHeader;
+            if (this.props.headerPolicy) { header = this.props.headerPolicy; }
+
+            var content = this.defaultContent;
+            if (this.props.contentPolicy) { content = this.props.contentPolicy; }
+
+            var footer = this.defaultFooter;
+            if (this.props.footerPolicy) { footer = this.props.footerPolicy; }
+
+            header.bind(this)(children);
+            content.bind(this)(children);
+            footer.bind(this)(children);
 
             return (
-                <li key={this.props.key} className={this.props.className}>
+                <li key={this.props.key} className={this.className()}>
                     {children}
                 </li>
             );
