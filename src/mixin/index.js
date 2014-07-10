ff0d5706477738cf62caef57428c99de0e3949da
Tim Popham <popham@uw.edu>
2014-07-09 19:40:03 -0700

    Literate name, cleaner export, and obsessive naming

--- src/mixin/index.js
+++
@@ -1,9 +0,0 @@
-define(['./draggable', './groupMember', './panel', './panelsPublish'], function (
-           draggable,     groupMember,     panel,     panelsPublish) {
-    return {
-        draggable : draggable,
-        groupMember : groupMember,
-        panel : panel,
-        panelsPublish : panelsPublish
-    };
-});
