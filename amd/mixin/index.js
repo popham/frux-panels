283179df3e3cfbb670fb6e375785fb99d37c972b
Tim Popham <popham@uw.edu>
2014-07-14 12:43:39 -0700

    Namespace

---
+++ amd/mixin/index.js
@@ -0,0 +1,10 @@
+define(['./drag', './panel', './panelsPublish', './storeItemExclusions'], function (
+           drag,     panel,     panelsPublish,     storeItemExclusions) {
+
+    return {
+        drag : drag,
+        panel : panel,
+        panelsPublish : panelsPublish,
+        storeItemExclusions : storeItemExclusions
+    };
+});
