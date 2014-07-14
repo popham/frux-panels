66f00af401e5fa449333bb1107e4f56c545a5f50
Tim Popham <popham@uw.edu>
2014-07-14 12:14:09 -0700

    Syntax

--- amd.js
+++ amd.js
@@ -1,5 +1,5 @@
 define(['./lib/DragBus', './lib/Insertion', './lib/Store', './lib/install/group', './lib/install/orphanage', './lib/mixin/index'], function (
-               DragBus,         Insertion,         Store,                 group,                 orphanage          mixin){
+               DragBus,         Insertion,         Store,                 group,                 orphanage,         mixin){
 
     return {
         DragBus : DragBus,
