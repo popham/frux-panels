6ca79886944aaa659c78fb62654d33c4ecfc15dc
Tim Popham <popham@uw.edu>
2014-07-16 14:30:46 -0700

    Repair normalize.css build

--- scripts/css.sh
+++ scripts/css.sh
@@ -1,8 +1,8 @@
 cp less/variables.less node_modules/bootstrap/less
 
 node ./node_modules/.bin/lessc --include-path=./node_modules \
-    less/basic.less css/basic.css & \
+    less/basic.less css/basic.css &
 node ./node_modules/.bin/lessc --include-path=./node_modules \
-    less/glyphicons.less css/glyphicons.css & \
+    less/glyphicons.less css/glyphicons.css &
 node ./node_modules/.bin/lessc --include-path=./node_modules/bootstrap/less \
-    ./node_modules/bootstrap/less/normalize.less css/normalize.css
+    ./node_modules/bootstrap/less/normalize.less css/normalize.css &
